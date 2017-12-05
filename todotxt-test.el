b4e01dbaf6f0ae7fd05eed7d12058bc6e9f348f1
nkenbou <nkenbou@gmail.com>
2017-12-05 23:49:59 +0900

    Add todotxt-transpose-lines-(up|down)

--- todotxt-test.el
+++ todotxt-test.el
@@ -38,3 +38,182 @@
          (expected '("@context1" "@context2" "@context3" "+project1" "+project2")))
     (should (equal (sort (todotxt-get-tag-completion-list-from-string str) 'string>)
                    (sort expected 'string>)))))
+
+(ert-deftest todotxt-test-transpose-lines-up-at-top ()
+  (with-temp-buffer
+    (insert "abc
+def
+ghi
+")
+    (goto-char (point-min))
+    (todotxt-transpose-lines-up)
+    (should (equal "abc
+def
+ghi
+"
+                   (buffer-string)))
+    (should (equal nil (char-before)))
+    (should (equal ?a (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-up-without-priority ()
+  (with-temp-buffer
+    (insert "abc
+def
+ghi
+")
+    (goto-char (point-min))
+    (forward-line)
+    (forward-char)
+    (todotxt-transpose-lines-up)
+    (should (equal "def
+abc
+ghi
+"
+                   (buffer-string)))
+    (should (equal nil (char-before)))
+    (should (equal ?d (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-up-with-higher-priority ()
+  (with-temp-buffer
+    (insert "(A) abc
+def
+ghi
+")
+    (goto-char (point-min))
+    (forward-line)
+    (forward-char)
+    (todotxt-transpose-lines-up)
+    (should (equal "(A) abc
+def
+ghi
+"
+                   (buffer-string)))
+    (should (equal ?d (char-before)))
+    (should (equal ?e (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-up-with-same-priority ()
+  (with-temp-buffer
+    (insert "(A) abc
+(A) def
+ghi
+")
+    (goto-char (point-min))
+    (forward-line)
+    (forward-char)
+    (todotxt-transpose-lines-up)
+    (should (equal "(A) def
+(A) abc
+ghi
+"
+                   (buffer-string)))
+    (should (equal nil (char-before)))
+    (should (equal ?\( (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-up-in-filterd-buffer ()
+  (with-temp-buffer
+    (insert "abc +a
+def
+ghi +a
+")
+    (todotxt-filter (eval `(lambda () (not (todotxt-current-line-match "+a")))))
+    (todotxt-apply-active-filters)
+    (goto-char (point-min))
+    (forward-line)
+    (todotxt-transpose-lines-up)
+    (todotxt-unhide-all)
+    (should (equal "ghi +a
+abc +a
+def
+"
+                   (buffer-string)))
+    (should (equal nil (char-before)))
+    (should (equal ?g (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-down-at-bottom ()
+  (with-temp-buffer
+    (insert "abc
+def
+ghi
+")
+    (goto-char (point-max))
+    (todotxt-transpose-lines-down)
+    (should (equal "abc
+def
+ghi
+"
+                   (buffer-string)))
+    (should (equal ?\C-j (char-before)))
+    (should (equal nil (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-down-without-priority ()
+  (with-temp-buffer
+    (insert "abc
+def
+ghi
+")
+    (goto-char (point-min))
+    (forward-line)
+    (forward-char)
+    (todotxt-transpose-lines-down)
+    (should (equal "abc
+ghi
+def
+"
+                   (buffer-string)))
+    (should (equal ?\C-j (char-before)))
+    (should (equal ?d (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-down-with-higher-priority ()
+  (with-temp-buffer
+    (insert "abc
+def
+(A) ghi
+")
+    (goto-char (point-min))
+    (forward-line)
+    (forward-char)
+    (todotxt-transpose-lines-down)
+    (should (equal "abc
+def
+(A) ghi
+"
+                   (buffer-string)))
+    (should (equal ?d (char-before)))
+    (should (equal ?e (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-down-with-same-priority ()
+  (with-temp-buffer
+    (insert "abc
+(A) def
+(A) ghi
+")
+    (goto-char (point-min))
+    (forward-line)
+    (forward-char)
+    (todotxt-transpose-lines-down)
+    (should (equal "abc
+(A) ghi
+(A) def
+"
+                   (buffer-string)))
+    (should (equal ?\C-j (char-before)))
+    (should (equal ?\( (char-after)))))
+
+(ert-deftest todotxt-test-transpose-lines-down-in-filterd-buffer ()
+  (with-temp-buffer
+    (insert "abc +a
+def
+ghi +a
+")
+    (todotxt-filter (eval `(lambda () (not (todotxt-current-line-match "+a")))))
+    (todotxt-apply-active-filters)
+    (goto-char (point-min))
+    (todotxt-transpose-lines-down)
+    (todotxt-unhide-all)
+    (should (equal "def
+ghi +a
+abc +a
+"
+                   (buffer-string)))
+    (should (equal ?\C-j (char-before)))
+    (should (equal ?a (char-after)))))
